8a87c7685671257897a426d680611190a5496b02
Dustin Sampson <gridcell@users.noreply.github.com>
2022-03-31 15:28:27 -0700

    Upgrade boto3 version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name="mermaid-changelog",
     description="",
-    version="0.2.2",
+    version="0.2.3",
     py_modules=["mermaid_changelog"],
-    install_requires=["boto3==1.9.195", "py-trello==0.15.0"],
+    install_requires=["boto3==1.21.31", "py-trello==0.15.0"],
     entry_points="""
         [console_scripts]
         chlog=mermaid_changelog:main
